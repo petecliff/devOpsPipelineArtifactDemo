8ece77c1d55deae916d222ea658370077f638ac7
Peter Cliff <pete@petecliff.net>
2022-08-10 16:55:48 +0100

    Fix python error

--- build.py
+++ build.py
@@ -24,8 +24,8 @@ files = []
 
 for r,d,f in os.walk("."):
     for file in f:
-        if !('.git' in file):
-          files.append(os.path.join(r, file))
+        if ('.git' not in file):
+            files.append(os.path.join(r, file))
 
 for f in files:
     print(f)
